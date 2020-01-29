30544e8095d75389adcb2b8c1be94b0c82625688
Avyay Natarajan <avyaysbhs@gmail.com>
2020-01-29 11:04:50 -0500

    Day 3 with project. i fixed messed up vcs changes #1

--- app/src/main/java/com/example/geocoderapp/MainActivity.java
+++ app/src/main/java/com/example/geocoderapp/MainActivity.java
@@ -75,8 +75,8 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
 
         if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             requestPermissions(
-                new String[] { Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION },
-                requestId++
+                    new String[] { Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION },
+                    requestId++
             );
             return;
         }
